10b11805944b0c7d8d11981e5cc830a493a98978
Ross Jaeger <rossjaeger@outlook.com>
2021-09-03 13:18:24 -0400

    Updated mac startup script

--- startup.sh
+++ startup.sh
@@ -5,16 +5,16 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 chmod +x /users/ross/scripts/startup.sh
-echo -e "\n\n"
-echo -e "${GREEN}Checking for updates...${NC}"
+echo "\n\n"
+echo "${GREEN}Checking for updates...${NC}"
 echo "-----------------------------------------"
 brew update > output.txt
 cat  output.txt
-echo -e "\n\n"
+echo "\n\n"
 
-echo -e "${GREEN}Checking for upgrades...${NC}"
+echo "${GREEN}Checking for upgrades...${NC}"
 echo "-----------------------------------------"
 brew upgrade > output.txt
 cat output.txt
-echo -e "\n\n"
+echo "\n\n"
 
